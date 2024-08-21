a5c1faf23a86fbf63cd7da27158a4d81e8925df7
arch-bert <archbert66@gmail.com>
2024-08-21 19:30:39 +0200

    Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Scan your React files for Tailwind
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
