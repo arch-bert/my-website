e7139533a7d0f33e489950e5911156a94e77d6dd
arch-bert <archbert66@gmail.com>
2024-08-21 20:55:45 +0200

    Basic hero component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import NavBar from './components/nav-bar';
+import Hero from './components/hero';
 
 export default function App() {
   return (
-    <div  className='text-3xl'>
-      <h1>Hello, world!</h1>
+    <div className='bg-black'>
+      <NavBar />
+      <Hero />
+      <div  className='text-3xl'>
+        <h1>Hello, world!</h1>
+      </div>
     </div>
   );
 }
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,11 +1,12 @@
 export default function NavBar() {
-    return (
-        <nav>
-        <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        </nav>
-    );
-    }
+  return (
+    <nav className="flex flex-row justify-between py-3 px-3 bg-transparent text-white">
+      <h1 className="w-[600px]">My Site</h1>
+      <ul className="flex flex-row space-x-4">
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
