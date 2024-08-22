6fae266ec7d7598ce63a7a1057c49253e234fc1e
arch-bert <archbert66@gmail.com>
2024-08-22 20:35:30 +0200

    Added font: Source Code

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         primary: '#000000',
         secondary: '#1c1c1c',
       },
+      fontFamily: {
+        source: ['Source Code Pro', 'monospace'], // Add Source Code Pro
+      },
     },
   },
   plugins: [],
