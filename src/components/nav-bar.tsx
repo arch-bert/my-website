ca8823c551301e9efde928d225fad742c8d40e4e
arch-bert <archbert66@gmail.com>
2024-08-22 20:34:47 +0200

    Added more space between nav components

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,7 +2,7 @@ export default function NavBar() {
   return (
     <nav className="flex flex-row justify-between py-3 px-3 bg-transparent text-white">
       <h1 className="w-[600px]">My Site</h1>
-      <ul className="flex flex-row space-x-4">
+      <ul className="flex flex-row space-x-10">
         <li>Projects</li>
         <li>About</li>
         <li>Contact</li>
