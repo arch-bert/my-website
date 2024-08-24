2391942ac48f3b02b56e8025e2bf32fd157978e8
arch-bert <archbert66@gmail.com>
2024-08-25 00:49:28 +0200

    Added headshot to profile-card

---
+++ src/components/profile-card.tsx
@@ -0,0 +1,7 @@
+export default function ProfileCard() {
+  return (
+    <div className="flex flex-col rounded-xl bg-secondary">
+        <img src="me.png" alt="Display Photo" />
+    </div>
+  );
+}
