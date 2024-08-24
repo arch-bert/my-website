d6f2499d67e39033adf8418daaffa0622266535d
arch-bert <archbert66@gmail.com>
2024-08-25 01:56:43 +0200

    Improved profilecard

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -1,7 +1,10 @@
 export default function ProfileCard() {
   return (
-    <div className="flex flex-col w-[400px] rounded-xl bg-secondary">
-        <img src="me.jpg" alt="Display Photo" />
+    <div className="flex flex-col p-8 items-center w-[400px] rounded-xl bg-secondary">
+        <div className="pb-8">
+            <img src="me.jpg" alt="Display Photo" />
+        </div>
+        <h2 className="font-alpha text-3xl">Ryan Bertschinger</h2>
     </div>
   );
 }
