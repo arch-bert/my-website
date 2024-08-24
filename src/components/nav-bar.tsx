82e314b8d81eda1f8e194595a10451e4c80ed1af
arch-bert <archbert66@gmail.com>
2024-08-24 18:37:17 +0200

    Design improvements

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,15 +1,23 @@
 export default function NavBar() {
   return (
-    <nav className="flex flex-row justify-between py-3 px-3 bg-transparent text-white">
+    <nav className="flex flex-row justify-between py-3 px-3 bg-transparent text-white font-alphas">
       <h1 className="w-[600px]">My Site</h1>
       <ul className="flex flex-row space-x-10">
         <li>
-          <a href="#projects">Projects</a>
+          <a href="#projects" className="relative after:absolute after:left-0 after:right-0 after:bottom-[-4px] after:h-[3px] after:bg-white after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a href="#about" className="relative after:absolute after:left-0 after:right-0 after:bottom-[-4px] after:h-[3px] after:bg-white after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">
+            About
+          </a>
         </li>
-        <li>Contact</li>
+        <li>
+          <a href="#contact" className="relative after:absolute after:left-0 after:right-0 after:bottom-[-4px] after:h-[3px] after:bg-white after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">
+            Contact
+          </a>
+        </li>
       </ul>
     </nav>
   );
