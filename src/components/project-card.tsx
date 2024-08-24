de145e3edbbfeda4a31e180c20f822142ee4884b
arch-bert <archbert66@gmail.com>
2024-08-24 21:37:07 +0200

    Removed coments

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -4,10 +4,8 @@ export default function ProjectCard() {
   return (
     <div className="pt-12">
       <div className="flex flex-row h-[300px] px-4 pt-4 rounded-xl bg-secondary outline outline-1 outline-gray-600 shadow-[5px_7px_10px_rgba(255,255,255,0.3)]">
-        {/* Column container with height matching the row */}
         <div className="flex flex-col h-full">
           <h1 className="text-3xl pb-2">Card Counting Simulator</h1>
-          {/* Image with max height set */}
           <img src="simulation.png" alt="Code" className="max-h-[80%] object-contain rounded-xl" />
         </div>
         <div className="flex flex-col h-full w-1/2">
