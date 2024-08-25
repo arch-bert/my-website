3c385acd67977df7e5639921908fca05ac51ed9c
arch-bert <archbert66@gmail.com>
2024-08-25 20:43:21 +0200

    Personalised hero text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,14 +11,13 @@ export default function Hero() {
         className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
       >
         <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          The hell is this?
+          Need a Software Engineer?
         </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-          burned and you&apos;ll have a scar.
-        </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 my-button">Watch trailer</button>
+        <div className="flex flex-row items-center justify-center gap-3 mt-6">
+          <p className="text-white text-sm md:text-2xl text-center">
+            Say less,
+          </p>
+          <button className="px-4 py-2 my-button">Contact me</button>
         </div>
       </Vortex>
     </div>
