7ee3bdc3b66dde1fb1bda4240e745439ac7c5af6
arch-bert <archbert66@gmail.com>
2024-08-25 14:57:05 +0200

    format changes

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -2,13 +2,13 @@ import { BackgroundGradient } from "./ui/background-gradient";
 
 export default function ProfileCard() {
   return (
-        <BackgroundGradient>
-        <div className="flex flex-col p-8 items-center w-[400px] rounded-3xl bg-secondary my-shadow">
+    <BackgroundGradient>
+      <div className="flex flex-col p-8 items-center w-[400px] rounded-3xl bg-secondary my-shadow">
         <div className="pb-8 flex-shrink">
             <img src="me.jpg" alt="Display Photo" />
         </div>
         <h2 className="font-alpha text-3xl">Ryan Bertschinger</h2>
-        </div>
-        </BackgroundGradient>
+      </div>
+    </BackgroundGradient>
   );
 }
