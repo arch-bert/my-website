d4eb5e1e32e89831400eb171788b4184cd1a7689
arch-bert <archbert66@gmail.com>
2024-08-26 16:52:05 +0200

    Added my custom border

--- src/components/profile-card.tsx
+++ src/components/profile-card.tsx
@@ -1,6 +1,6 @@
 export default function ProfileCard() {
   return (
-    <div className="flex flex-col p-8 items-center w-[400px] rounded-3xl bg-secondary my-shadow">
+    <div className="flex flex-col p-8 items-center w-[400px] rounded-3xl bg-secondary my-shadow outline outline-4 outline-tertiary my-shadow hover:outline-white hover:scale-[1.05] hover:shadow-none transition-all duration-500 ease-in-out">
       <div className="pb-8 flex-shrink">
           <img src="me.jpg" alt="Display Photo" />
       </div>
