ba45b03ca7b5b5c15ea58fd794be231ab470a17a
ryan-bert <ryanberti66@gmail.com>
2024-09-21 23:42:18 +0200

    Project cards nearly done...

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -8,10 +8,10 @@ export interface ProjectCardProps {
   url: string;
 }
 
-export default function ProjectCard({title, img, tags, description, url}: ProjectCardProps) {
+export default function ProjectCard({ title, img, tags, description, url }: ProjectCardProps) {
   return (
-    <div className="flex flex-col md:flex-row h-auto p-4 md:p-8 mx-[5%] xl:mx-[10%] rounded-3xl bg-secondary outline outline-4 outline-tertiary my-shadow hover:outline-white hover:scale-[1.05] hover:shadow-none transition-all duration-500 ease-in-out">
-      <div className="flex flex-col justify-start mr-4">
+    <div className="flex flex-col md:flex-row h-auto p-4 md:p-8 mx-[5%] xl:mx-[10%] rounded-3xl bg-secondary outline outline-4 outline-tertiary my-shadow hover:outline-white hover:scale-[1.05] hover:shadow-none transition-all duration-500 ease-in-out overflow-hidden">
+      <div className="flex flex-col justify-start mr-4 flex-shrink-0">
         <h1 className="text-4xl font-bold pb-4 md:whitespace-nowrap">{title}</h1>
         <img
           src={img}
@@ -19,16 +19,16 @@ export default function ProjectCard({title, img, tags, description, url}: Projec
           className="object-cover rounded-xl max-w-full h-auto lg:max-w-[500px]"
         />
       </div>
-      <div className="flex flex-col pl-4 w-full space-y-6">
+      <div className="flex flex-col pl-4 w-full">
         <div className="ml-auto overflow-hidden pt-4 md:pt-0">
           <Tags text={tags} />
         </div>
-        <div className="flex-grow overflow-wrap break-words">
+        <div className="flex-grow flex items-center">
           <p className="text-white">
             {description}
           </p>
         </div>
-        <div className="ml-auto mt-4">
+        <div className="ml-auto mt-auto">
           <button className="w-fit px-4 py-2 my-button">
             <a href={url} target="_blank">View Project</a>
           </button>
