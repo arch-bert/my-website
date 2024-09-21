abcf36e3719d7cc1547e62227c21208b70fa34ba
ryan-bert <ryanberti66@gmail.com>
2024-09-21 20:37:04 +0200

    Made background white for bug fixing, will revert

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <>
-      <body className='bg-black pb-[1500px] text-white'>
+      <body className='bg-white text-white'>
         <NavBar />
         <Hero />
         <section id='projects' className="flex flex-col py-[60px] px-8">
