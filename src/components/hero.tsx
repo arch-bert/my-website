0680ebeed5d19c3a932fa081e60aa05ae10f6d89
ryan-bert <ryanberti66@gmail.com>
2024-09-21 21:25:51 +0200

    Removed padding and sizing of hero text

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,22 +13,20 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="flex flex-col justify-center items-center" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
+    <div style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
       <Vortex
         backgroundColor="black"
         rangeY={500}
         particleCount={1500}
         baseHue={0}
-        className="flex flex-col justify-center px-2 md:px-[10%] py-4 w-full h-full"
+        className="flex flex-col justify-center w-full h-full"
       >
-        <h2 className="text-white text-2xl md:text-7xl font-bold text-center">
+        <h2 className="text-white">
           <div>Need a Software</div>
           <div>Engineer?</div>
         </h2>
-        <div className="flex flex-col md:flex-row gap-4 mt-6">
-          <p className="text-white text-sm md:text-2xl text-center">
-            Say less,
-          </p>
+        <div className="flex flex-row gap-4 mt-6 items-center">
+          <p>Say less,</p>
           <button className="px-4 py-2 my-button">Contact me</button>
         </div>
       </Vortex>
