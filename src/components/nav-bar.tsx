c6f052dcecd3ab5d4544a298cc35fff9f7ba9aa8
ryan-bert <ryanberti66@gmail.com>
2024-09-21 19:57:43 +0200

    responsiveness refactor complete

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,22 +1,16 @@
 export default function NavBar() {
   return (
-    <nav className="flex flex-row justify-between py-3 px-5 bg-transparent text-white font-semibold">
-      <h1 className="w-[600px]">My Site</h1>
-      <ul className="flex flex-row space-x-10">
+    <nav className="flex flex-row justify-between py-3 px-2 md:px-5 bg-transparent text-white font-semibold">
+      <a href="/" className="w-auto">My Site</a>
+      <ul className="flex flex-row space-x-5 md:space-x-10">
         <li>
-          <a href="#projects" className="my-underline">
-            Projects
-          </a>
+          <a href="#projects" className="my-underline">Projects</a>
         </li>
         <li>
-          <a href="#about" className="my-underline">
-            About
-          </a>
+          <a href="#about" className="my-underline">About</a>
         </li>
         <li>
-          <a href="#contact" className="my-underline">
-            Contact
-          </a>
+          <a href="#contact" className="my-underline">Contact</a>
         </li>
       </ul>
     </nav>
