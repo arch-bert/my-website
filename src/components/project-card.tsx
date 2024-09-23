a4f8e03cd74270c865dd705cdfefc4734e80aaa4
ryan-bert <ryanberti66@gmail.com>
2024-09-23 10:56:47 +0200

    Project cards = FULLY responsive

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -10,27 +10,27 @@ export interface ProjectCardProps {
 
 export default function ProjectCard({ title, img, tags, description, url }: ProjectCardProps) {
   return (
-    <div className="flex flex-col md:flex-row h-auto p-4 md:p-8 mx-[5%] xl:mx-[10%] rounded-3xl bg-secondary outline outline-4 outline-tertiary my-shadow hover:outline-white hover:scale-[1.05] hover:shadow-none transition-all duration-500 ease-in-out overflow-hidden">
-      <div className="flex flex-col justify-start mr-4 flex-shrink-0">
-        <h1 className="text-3xl lg:text-4xl font-bold pb-4 md:whitespace-nowrap">{title}</h1>
+    <div className="flex flex-col lg:flex-row h-auto p-4 lg:p-8 mx-[5%] xl:mx-[10%] rounded-3xl bg-secondary outline outline-4 outline-tertiary my-shadow hover:outline-white hover:scale-[1.05] hover:shadow-none transition-all duration-500 ease-in-out overflow-hidden">
+      <div className="flex flex-col justify-start lg:mr-4 flex-shrink-0">
+        <h1 className="text-3xl lg:text-4xl font-bold pb-4 lg:whitespace-nowrap">{title}</h1>
         <img
           src={img}
-          alt="Project Image"
-          className="object-cover rounded-xl h-auto md:max-w-[300px] lg:max-w-[400px] xl:max-w-[500px] max-h-60"
+          alt="Project Thumbnail"
+          className="object-cover rounded-xl max-w-full max-h-60 lg:max-w-[400px] xl:max-w-[500px] h-auto"
         />
       </div>
-      <div className="flex flex-col pl-4 w-full">
-        <div className="ml-auto overflow-hidden py-5 md:py-0">
+      <div className="flex flex-col w-full mt-4 lg:mt-0 lg:pl-4">
+        <div className="ml-auto py-3">
           <Tags text={tags} />
         </div>
-        <div className="flex-grow flex lg:items-center">
+        <div className="flex-grow lg:flex lg:items-center overflow-hidden">
           <p className="text-white">
             {description}
           </p>
         </div>
-        <div className="ml-auto mt-auto">
-          <button className="w-fit mt-4 md:mt-0 px-4 py-2 my-button">
-            <a href={url} target="_blank">View Code</a>
+        <div className="ml-auto mt-4 lg:mt-auto">
+          <button className="w-fit px-4 py-2 my-button">
+            <a href={url} target="_blank" rel="noreferrer">View Code</a>
           </button>
         </div>
       </div>
