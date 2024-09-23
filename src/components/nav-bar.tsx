322f7d57f7fd0547da2754d47726103acfb90d6f
ryan-bert <ryanberti66@gmail.com>
2024-09-23 17:49:22 +0200

    Added logo to nav bar

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,8 +1,10 @@
 export default function NavBar() {
   return (
-    <nav className="flex flex-row justify-between py-3 px-2 md:px-5 bg-black text-white font-semibold">
-      <a href="/" className="w-auto">My Site</a>
-      <ul className="flex flex-row space-x-5 md:space-x-10">
+    <nav className="flex flex-row justify-between px-2 md:px-5 bg-black text-white font-semibold">
+      <a href="/" className="w-[40px] my-1">
+        <img src="logo.png" alt="RB" />
+      </a>
+      <ul className="flex flex-row space-x-5 md:space-x-10 my-3">
         <li>
           <a href="#projects" className="my-underline">Projects</a>
         </li>
